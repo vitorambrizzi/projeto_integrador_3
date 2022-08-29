70ebabdd0f504da33fae9b0571f0cd0c37b114e0
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-08-29 17:23:18 -0300

    created logo and menu div

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,12 @@ import "./Header.css";
 
 const Header = () => {
   return (
-   <header className='header-nav'>
-
-   </header>
+    <header id='Header'>
+      <div className='container'>
+        <div>Logo</div>
+        <div>Menu</div>
+      </div>
+    </header>
   )
 }
 
