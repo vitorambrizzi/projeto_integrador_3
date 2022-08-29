325fe91d268442e13c51ef89ed8e7f5d9975c08b
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-08-29 17:53:55 -0300

    added Contact page links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 }
