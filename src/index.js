597fcf3b1ad07ae088d6f18d971a9d4a65d37ac4
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-08-29 17:15:58 -0300

    minor index changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.js";
 
-// Render react application in root div from public/index.html 
+// Render react in root div from public/index.html 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
