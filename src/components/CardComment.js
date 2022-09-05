507b7ebf75831975a5c9499ddd0bd4207b32620b
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-09-05 17:52:25 -0300

    Started CardComment component

---
+++ src/components/CardComment.js
@@ -0,0 +1,47 @@
+const CardComment = () => {
+  return (
+    <div>
+        <div></div>
+        <div>
+            <h1></h1>
+            <p></p>
+        </div>
+    </div>
+  )
+}
+
+const style = {
+    avatar: {
+        background: '#CCC',
+        borderRadius: '50%',
+        height: '50px',
+        width: '50px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: '0 10px 0 0'
+    },
+
+    card: {
+        width: '300px',
+        background: '#EEE',
+        display: 'flex',
+        padding: '10px',
+        borderRadius: '10px',
+        margin: '15px'
+    },
+
+    userName: {
+
+    },
+
+    text: {
+
+    },
+
+    box: {
+
+    }
+}
+
+export default CardComment
