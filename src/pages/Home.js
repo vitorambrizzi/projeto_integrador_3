e1d4ddbce679e661b3a6df62f68fa4b1932a2530
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-10-03 15:59:44 -0300

    Handle Modal edit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,28 @@ import {API_PATH} from '../config'
 import Header from '../components/Header'
 import MainContainer from '../components/MainContainer'
 import CardUser from '../components/CardUser'
+import Modal from '../components/Modal'
 import Footer from '../components/Footer'
 
 const Home = () => {
   const [users, setUsers] = useState([])
+  const [userToEdit, setUserToEdit] = useState({
+    id: '',
+    name: '',
+    email: '',
+    avatar: ''
+  })
+  const [showModal, setShowModal] = useState(false)
 
   const requestUsers = async () => {
     const response = await fetch(`${API_PATH}user/list`)
     const result = await response.json()
     console.log(result.success.message)
     setUsers(result.data)
+  }
+
+  const handleChange = (event) => {
+    setUserToEdit({...userToEdit, [event.target.name]: event.target.value})
   }
 
   useEffect( () => {
@@ -24,7 +36,6 @@ const Home = () => {
       <Header />
       <MainContainer>
         <h1>Home</h1>
-
         <p>API User list:</p>
         {  
           users.length === 0
@@ -33,7 +44,7 @@ const Home = () => {
           :
             users.map((user) => {
               return (
-                <CardUser users={users} setUsers={setUsers} key={user.id} avatarUrl={user.avatar} name={user.name} id={user.id}>
+                <CardUser users={users} setUsers={setUsers} key={user.id} avatarUrl={user.avatar} name={user.name} id={user.id} setShowModal={setShowModal} setUserToEdit={setUserToEdit}>
                   {user.email}
                 </CardUser>
               )
@@ -41,6 +52,19 @@ const Home = () => {
         }
       </MainContainer>
       <Footer />
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <h1>Edit User</h1>
+        <form>
+          <input type='hidden' name='id' value={userToEdit.id} />
+          <p>Name: <input type='text' name='name' value={userToEdit.name} onChange={(event) => handleChange(event)}
+          /></p>
+          <p>Email: <input type='text' name='email' value={userToEdit.email} onChange={(event) => handleChange(event)}
+          /></p>
+          <p>Avatar: <input type='text' name='avatar' value={userToEdit.avatar} onChange={(event) => handleChange(event)}
+          /></p>
+          <input type='submit' value='send' />
+        </form>
+      </Modal>
     </>
   )
 }
