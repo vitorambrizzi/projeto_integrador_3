3843615cba7a21bbb2100da01a76b5299954f106
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-10-25 17:45:55 -0300

    Started to develop an initialization component

---
+++ src/components/Initialization.js
@@ -0,0 +1,13 @@
+import {useState, useEffect} from 'react'
+import styled from 'styled-components'
+import useAuth from '../hooks/useAuth'
+import Loader from './Loader'
+
+const Initialization = ({children}) => {
+    
+    return (
+        <div>Initialization</div>
+    )
+}
+
+export default Initialization
