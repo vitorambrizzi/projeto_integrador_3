73bdfd6cb3c6b8dae1ea02d9d0ac944709070a3f
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-10-25 16:38:40 -0300

    Fixed authprovider destruct

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -2,7 +2,7 @@ import {createContext, useState} from 'react'
 
 export const AuthContext = createContext()
 
-const AuthProvider = (children) => {
+const AuthProvider = ({children}) => {
     const [userLogged, setUserLogged] = useState({
         isLogged: false,
         idUser: '',
