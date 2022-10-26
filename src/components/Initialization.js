cb0f2bd9acac6440c2f62b1bffabb01fe5e1e813
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-10-26 15:00:34 -0300

    Finished initialization component

--- src/components/Initialization.js
+++ src/components/Initialization.js
@@ -4,10 +4,43 @@ import useAuth from '../hooks/useAuth'
 import Loader from './Loader'
 
 const Initialization = ({children}) => {
-    
+    const [firstLoad, setFirstLoad] = useState(true)
+    const [, setUserLogged] = useAuth()
+
+    useEffect(() => {
+        const auth = JSON.parse(localStorage.getItem('user-auth'))
+        if (auth?.isLogged) {
+            setUserLogged(auth)
+        }
+        setTimeout(() => {
+            setFirstLoad(false)
+        }, 2000)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
-        <div>Initialization</div>
+        <>
+            {
+                firstLoad ?
+                    <Loading><p><Loader /></p><br /><br /><p>Loading...</p></Loading>
+                :
+                    children
+            }
+        </>
     )
 }
 
 export default Initialization
+
+const Loading = styled.div`
+    display: flex;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: #cccccc;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
