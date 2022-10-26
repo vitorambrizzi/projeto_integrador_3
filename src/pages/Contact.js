ccc4c63948fb18d18100675006f51f622d2c5c13
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-10-26 15:00:16 -0300

    Removed useContext example

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,9 @@
 import {useState, useEffect} from 'react'
-import {AuthContext} from '../providers/AuthProvider'
-import {useContext} from 'react'
 
 const Contact = () => {
     // useState example
     const [nameUser, setNameUser] = useState("Vítor")
     const [count, setCount] = useState(0)
-    const [userLogged, setUserLogged] = useContext(AuthContext)
 
     // useEffect example
     useEffect( () => {
@@ -19,8 +16,6 @@ const Contact = () => {
         <button onClick={() => setNameUser("Renan")}>Change name to Renan</button>
         <br/>
         <button onClick={() => setNameUser("Vítor")}>Change name back to Vítor</button>
-        <br/>
-        <button onClick={() => setUserLogged({...userLogged, idUser: '20'})}>Login with id = 20</button>
         <br/>
         <h1>My number is: {count}.</h1>
         <button onClick={() => setCount(count + 1)}>Plus 1</button>
